21dd5d41e20113b5bec78eba6366c6a746d65850
dhontavo <gast98.adolfo@gmail.com>
2026-06-29 16:24:47 -0700

    feat: initialize catalog component, define API environment configuration, and set up application routing paths

--- catalog-web/src/app/app.routes.ts
+++ catalog-web/src/app/app.routes.ts
@@ -3,5 +3,8 @@ import { CatalogComponent } from './components/catalog/catalog.component';
 
 export const routes: Routes = [
   { path: '', component: CatalogComponent },
+  { path: 'store/:id_store', component: CatalogComponent },
+  { path: ':id_store', component: CatalogComponent },
   { path: '**', redirectTo: '' },
 ];
+
